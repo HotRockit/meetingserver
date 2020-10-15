d6b0643f7d661e6181b2ea947aec26fd67a9faed
HotRockit <wdy1607020322@gmail.com>
2020-10-15 18:50:20 +0800

    添加账号状态

---
+++ dao/dao.go
@@ -0,0 +1,95 @@
+package dao
+
+import (
+	"log"
+	"meetingserver/model"
+	"meetingserver/util"
+)
+
+type AppDao struct {
+	*util.GormEngine
+}
+
+func (appDao *AppDao) LoginDao(username string,password string) model.User {
+	var user model.User
+	appDao.Where("username = ?",username).Where("password = ?",password).First(&user)
+
+	//可以使用下面这种方式判断结构体是否为空，但这个时候要求结构体的所有字段都是可比较的
+	//if user == (model.User{}){
+	//	return false
+	//}
+
+	//下面这种反射的方式就没有上面方法的局限性，但是反射的性能较低
+	if user.UserIsEmpty(){
+		appDao.Where("phone = ?",username).Where("password = ?",password).First(&user)
+		if user.UserIsEmpty(){
+			return user
+		}
+		return user
+	}
+	return  user
+}
+
+func (appDao *AppDao) RegisterDao(username string, password string, phone string) uint {
+	//首先判断用户名是否存在
+	var user model.User
+	appDao.Where("username = ?",username).First(&user)
+	if !user.UserIsEmpty(){
+		log.Print("用户存在")
+		return 0  //用户存在
+	}
+	if err := appDao.Create(&model.User{Username: username,Password: password,Phone: phone}).Error;err !=nil{
+		log.Print("插入失败")
+		return 1
+	}
+	return  2
+}
+
+func (appDao *AppDao) AddMeetingDao(name string, user string,start int,end int, reason string,borrow string) uint {
+	if err := appDao.Create(&model.Meeting{MeetingRoomName: name,User: user,StartTime: start,
+		EndTime: end,Reason: reason,BorrowTime: borrow}).Error;err !=nil{
+		log.Print("插入失败")
+		return 0
+	}
+	return  1
+}
+
+func (appDao *AppDao) QueryMeetingDao1(user string,day string,room string) []model.Meeting {
+	var meetings []model.Meeting
+	if err := appDao.Where("user = ?",user).Where("borrow_time = ?",day).Where("meeting_room_name = ?",room).Find(&meetings).Error; err != nil{
+		log.Print("获取失败")
+		return nil
+	}else {
+		return meetings
+	}
+}
+
+func (appDao *AppDao) QueryMeetingDao2(day string, room string) []model.Meeting{
+	var meetings []model.Meeting
+	if err := appDao.Where("borrow_time = ?",day).Where("meeting_room_name = ?",room).Find(&meetings).Error; err != nil{
+		log.Print("获取失败")
+		return nil
+	}else {
+		return meetings
+	}
+}
+
+func (appDao *AppDao) UpdatePasswordDao(username string, password string) uint {
+	var user model.User
+	appDao.Where("username = ?",username).First(&user)
+	if err := appDao.Model(&user).Update("password",password).Error;err != nil{
+		log.Print("更新失败")
+		return 0
+	}
+	return 1
+}
+
+func (appDao *AppDao) DeleteUserDao(username string) uint {
+	//删除用户的同时把他对应的会议记录一同删除
+	if err := appDao.Where("username = ?",username).Delete(&model.User{}).Error;err != nil{
+		log.Print("删除失败")
+		return 0
+	}
+	appDao.Where("user = ?",username).Delete(&model.Meeting{})
+	return 1
+}
---
+++ model/model.go
@@ -0,0 +1,44 @@
+package model
+
+import (
+	"reflect"
+)
+
+//Meeting is a struct to describe meeting
+type Meeting struct {
+	MeetingID       uint `json:"meeting_id" gorm:"primary_key;not null;auto_increment"`
+	MeetingRoomName string `json:"meeting_room_name" gorm:"not null;type:varchar(100)"`
+	User            string `json:"user" gorm:"not null;type:varchar(100)"`
+	StartTime       int `json:"start_time" gorm:"type:int;not null"`    //这里为了简化操作，所有的时间类型全部都转换成了string类型
+	EndTime         int `json:"end_time" gorm:"type:int;not null"`   //这里为了和微信小程序对应，数据库里面其实没有存时间，存的是方格的索引值
+	Reason          string `json:"reason" gorm:"not null;type:varchar(100)"`
+	BorrowTime      string `json:"borrow_time" gorm:"type:varchar(20);not null"`  //格式为 2020-06-03go
+}
+
+//User is a struct to describe user
+type User struct {
+	//UserID uint `json:"user_id" gorm:"primary_key;not null;auto_increment"`
+	Username string `json:"username" gorm:"primary_key;type:varchar(100)"`
+	Password string `json:"password" gorm:"not null;type:varchar(100)"`
+	Phone    string `json:"phone" gorm:"not null;type:varchar(15)"`
+	State	int `json:"state" gorm:"not null;type:int;default:0"`    //0代表状态正常,1代表账号被冻结了
+}
+
+//MeetingRoom is a struct to describe MeetingRoom
+type MeetingRoom struct {
+	//RoomID uint `json:"room_id" gorm:"primary_key;not null;auto_increment"`
+	RoomName string `json:"room_name" gorm:"primary_key;type:varchar(100)"`
+}
+
+//判断结构体是否为空
+func (user User) UserIsEmpty() bool {
+	return reflect.DeepEqual(user,User{})
+}
+
+func (meeting Meeting) MeetingIsEmpty() bool {
+	return reflect.DeepEqual(meeting,Meeting{})
+}
+
+func (room MeetingRoom) MeetingRoomIsEmpty() bool {
+	return reflect.DeepEqual(room,MeetingRoom{})
+}
